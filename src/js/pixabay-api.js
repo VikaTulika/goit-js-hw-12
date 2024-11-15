a0d6fb510cf2d631d8e4c2affe3f5a4b7bd33ce5
VikaTulika <mariyaviktoriyatulika@gmail.com>
2024-11-15 11:53:32 +0100

    first commit

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,12 +1,7 @@
-export function fetchFotos(query) {
+import axios from "axios";
+
+export async function fetchFotos(query) {
     const BASEURL = "https://pixabay.com/api/";
     const API_KEY = "47091591-3a0c1be132fb67c8f1fe23cd9";
-  return fetch(`${BASEURL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`).then(
-    (response) => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
+  return await axios(`${BASEURL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`)
     }
-  );
-}

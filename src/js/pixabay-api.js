b50cf653fae9c17039e2064621b71b795725981e
VikaTulika <mariyaviktoriyatulika@gmail.com>
2024-11-16 11:06:57 +0100

    final commit

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-export async function fetchFotos(query) {
+export async function fetchFotos(query, page) {
     const BASEURL = "https://pixabay.com/api/";
-    const API_KEY = "47091591-3a0c1be132fb67c8f1fe23cd9";
-  return await axios(`${BASEURL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`)
-    }
+  const API_KEY = "47091591-3a0c1be132fb67c8f1fe23cd9";
+  const response = await axios(`${BASEURL}?key=${API_KEY}&q=${query}&page=${page}&per_page=15&image_type=photo&orientation=horizontal&safesearch=true`)
+  return response.data
+}
--- src/main.js
+++ src/main.js
@@ -8,19 +8,26 @@ import { fetchFotos } from "./js/pixabay-api";
 const form = document.querySelector(".search-form");
 const galleryList = document.querySelector(".gallery");
 const loader = document.querySelector(".loader")
+const loadMoreBtn = document.querySelector(".load-more-btn")
+let page = 1;
+let inputValue = "";
 
 const lightBox = new SimpleLightbox('.gallery a');
 form.addEventListener("submit", onSearchForm);
+loadMoreBtn.addEventListener("click", onLoadMore);
 function onSearchForm(event) {
     event.preventDefault();
-    const inputValue = form.elements.query.value.trim();
+    inputValue = form.elements.query.value.trim();
     if (inputValue === "") {
         iziToast.warning({message:"enter something for search"})
         return
     }
+
+    page = 1;
     galleryList.innerHTML = "";
     loader.classList.remove("hidden");
-    fetchFotos(inputValue).then(data => {
+    fetchFotos(inputValue, page).then(data => {
+        console.log(data);
         if (data.hits.length === 0) {
             iziToast.warning({message:"Sorry, there are no images matching your search query. Please try again!"})
             return 
@@ -28,6 +35,11 @@ function onSearchForm(event) {
         const markup = createGallery(data.hits)
         galleryList.insertAdjacentHTML("beforeend", markup)
         lightBox.refresh();
+        if (data.totalHits > 15) {
+            loadMoreBtn.classList.remove("hidden");
+        } else {
+            loadMoreBtn.classList.add("hidden");
+        }
     }).catch(error => {
         console.log(error)
         iziToast.error({message:"Oops, something went wrong!"});
@@ -35,4 +47,36 @@ function onSearchForm(event) {
         loader.classList.add("hidden")
         form.reset()
     })
+}
+
+async function onLoadMore() {
+    page += 1;
+    loader.classList.remove("hidden");
+    try {
+        const data = await fetchFotos(inputValue, page);
+        const markup = createGallery(data.hits);
+        galleryList.insertAdjacentHTML("beforeend", markup);
+        lightBox.refresh();
+        scroll();
+        if (Math.ceil(data.totalHits / 15) >page) {
+            loadMoreBtn.classList.remove("hidden");
+        } else {
+            loadMoreBtn.classList.add("hidden");
+            iziToast.info({message:"We're sorry, but you've reached the end of search results."})
+        }
+    } catch (error) {
+        console.log(error)
+        iziToast.error({message:"Oops, something went wrong!"});
+    }
+    finally {
+        loader.classList.add("hidden");
+    }
+}
+function scroll() {
+    const cardHeigth = document.querySelector(".item").getBoundingClientRect().height
+    window.scrollBy({
+    top: cardHeigth * 2,
+    behavior: "smooth",
+});
+
 }
